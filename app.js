d212f4fddab71ba32b71e0ad9a38fdbf8b834167
Donghak Oh <d4oh@ucsd.edu>
2018-11-03 23:57:11 -0700

    little changes

--- app.js
+++ app.js
@@ -16,23 +16,6 @@ function welcome() {
     alert("반갑습니다.")
 }
 
-function date() {
-    var today = new Date()
-    var dd = today.getDate()
-    var mm = today.getMonth() + 1
-    var yyyy = today.getFullYear()
-    
-    if(dd<10) {
-        dd = '0' + dd
-    }
-    
-    if(mm<10) {
-        mm = '0' + mm
-    }
-    
-    today = mm+'/'+dd+'/'+yyy
-    
-    alert("dlfkjdlkfj")
-    
-    documnet.write(today);
+function heaven() {
+    alert("2019년 6월 2일")
 }
