458868948e911e7bbcc627e7ff166d8012504c7b
Donghak Oh <d4oh@ucsd.edu>
2018-11-03 19:02:14 -0700

    mosol calculator

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0 // For Safari
+    document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
 }
 
 function submitPredictions() {
@@ -12,6 +12,27 @@ function showPredictions() {
     
 }
 
-function welcom() {
-    alert("반갑습니다.");
+function welcome() {
+    alert("반갑습니다.")
+}
+
+function date() {
+    var today = new Date()
+    var dd = today.getDate()
+    var mm = today.getMonth() + 1
+    var yyyy = today.getFullYear()
+    
+    if(dd<10) {
+        dd = '0' + dd
+    }
+    
+    if(mm<10) {
+        mm = '0' + mm
+    }
+    
+    today = mm+'/'+dd+'/'+yyy
+    
+    alert("dlfkjdlkfj")
+    
+    documnet.write(today);
 }
